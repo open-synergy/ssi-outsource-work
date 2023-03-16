7d2b228face78476feedce57813ea94712e87ffc
Miftahussalam <miftah_salam@ymail.com>
2023-03-16 14:31:17 +0700

    [ssi_outsource_work] 14.0.1.11.0

--- ssi_outsource_work/controllers/portal.py
+++ ssi_outsource_work/controllers/portal.py
@@ -21,6 +21,14 @@ class CustomerPortal(CustomerPortal):
                 )
                 else 0
             )
+        if "outsource_work_count" in counters:
+            values["outsource_work_count"] = (
+                request.env["outsource_work"].search_count([])
+                if request.env["outsource_work"].check_access_rights(
+                    "read", raise_exception=False
+                )
+                else 0
+            )
         return values
 
     def _outsource_work_outstanding_get_page_view_values(
@@ -69,7 +77,7 @@ class CustomerPortal(CustomerPortal):
                 ("create_date", "<=", date_end),
             ]
 
-        # project work outstanding count
+        # outsource work outstanding count
         outsource_work_outstanding_count = Outstanding.search_count(domain)
         # pager
         pager = portal_pager(
@@ -126,4 +134,109 @@ class CustomerPortal(CustomerPortal):
         values.update({"access_token": values.get("access_token", access_token)})
         return request.render(
             "ssi_outsource_work.portal_my_outsource_work_outstanding", values
+        )
+
+    def _outsource_work_get_page_view_values(
+        self, outsource_work, access_token, **kwargs
+    ):
+        values = {
+            "page_name": "outsource-works",
+            "outsource_work": outsource_work,
+        }
+        return self._get_page_view_values(
+            outsource_work,
+            access_token,
+            values,
+            "my_outsource_works_history",
+            False,
+            **kwargs
+        )
+
+    @http.route(
+        [
+            "/my/outsource-works",
+            "/my/outsource-works/page/<int:page>",
+        ],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def portal_my_outsource_works(
+        self, page=1, date_begin=None, date_end=None, sortby=None, **kw
+    ):
+        values = self._prepare_portal_layout_values()
+        OutsourceWork = request.env["outsource_work"]
+        domain = []
+
+        searchbar_sortings = {
+            "date": {"label": _("Newest"), "order": "create_date desc"},
+            "name": {"label": _("# Outsource Work"), "order": "name"},
+        }
+        if not sortby:
+            sortby = "date"
+        order = searchbar_sortings[sortby]["order"]
+
+        if date_begin and date_end:
+            domain += [
+                ("create_date", ">", date_begin),
+                ("create_date", "<=", date_end),
+            ]
+
+        # outsource work count
+        outsource_work_count = OutsourceWork.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/outsource-works",
+            url_args={"date_begin": date_begin, "date_end": date_end, "sortby": sortby},
+            total=outsource_work_count,
+            page=page,
+            step=self._items_per_page,
+        )
+
+        # content according to pager and archive selected
+        outsource_works = OutsourceWork.search(
+            domain, order=order, limit=self._items_per_page, offset=pager["offset"]
+        )
+        request.session[
+            "my_outsource_works_history"
+        ] = outsource_works.ids[:100]
+
+        values.update(
+            {
+                "date": date_begin,
+                "date_end": date_end,
+                "outsource_works": outsource_works,
+                "page_name": "outsource-works",
+                "default_url": "/my/outsource-works",
+                "pager": pager,
+                "searchbar_sortings": searchbar_sortings,
+                "sortby": sortby,
+            }
+        )
+        return request.render(
+            "ssi_outsource_work.portal_my_outsource_works", values
+        )
+
+    @http.route(
+        ["/my/outsource-work/<int:outsource_work_id>"],
+        type="http",
+        auth="public",
+        website=True,
+    )
+    def portal_my_outsource_work(
+        self, outsource_work_id=None, access_token=None, **kw
+    ):
+        try:
+            outsource_work_sudo = self._document_check_access(
+                "outsource_work", outsource_work_id, access_token
+            )
+        except (AccessError, MissingError):
+            return request.redirect("/my")
+
+        values = self._outsource_work_get_page_view_values(
+            outsource_work_sudo, access_token, **kw
+        )
+        values.update({"access_token": values.get("access_token", access_token)})
+        return request.render(
+            "ssi_outsource_work.portal_my_outsource_work", values
         )
